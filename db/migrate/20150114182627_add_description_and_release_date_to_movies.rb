84ffe6fc728ed7fda765453d6dafabf45644a97d
leary <ryan.leary@opower.com>
2015-01-14 10:44:54 -0800

    Initial commit. Section 6 of RoR training complete

---
+++ db/migrate/20150114182627_add_description_and_release_date_to_movies.rb
@@ -0,0 +1,6 @@
+class AddDescriptionAndReleaseDateToMovies < ActiveRecord::Migration
+  def change
+    add_column :movies, :description, :text
+    add_column :movies, :released_on, :date
+  end
+end
